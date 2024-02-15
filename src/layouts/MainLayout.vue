defb32d36e908f8cbbc2cfb4e33d8c2d5bb24f96
asmirnov <asmirnov@mobiledep.ru>
2024-02-15 20:09:47 +0300

    Этапы

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -17,10 +17,7 @@
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label header>
-          Essential Links
-        </q-item-label>
-
+        <q-item-label header> Этапы </q-item-label>
         <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
@@ -88,7 +85,7 @@ const essentialLinks: EssentialLinkProps[] = [
   },
 ];
 
-const leftDrawerOpen = ref(false);
+const leftDrawerOpen = ref<boolean>(false);
 
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
