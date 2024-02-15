a35388fad1d782b104ed94da45c3968aa1295dc5
asmirnov <asmirnov@mobiledep.ru>
2024-02-15 20:10:21 +0300

    Этапы испытания

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -17,7 +17,7 @@
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label header> Этапы </q-item-label>
+        <q-item-label header> Этапы испытания </q-item-label>
         <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
